42188b624a6e0ddd0da9b93a2dc32d12b0043615
Houguanglong <369380397@qq.com>
2019-03-23 22:16:21 +0800

    提交

---
+++ demo/server/websocket/Ws.php
@@ -0,0 +1,65 @@
+<?php
+namespace demo\server\websocket;
+/**
+ * User: websockte 基础类
+ * FileName: 文件名称
+ * Date: 2019/3/23
+ * Time: 21:42
+ */
+class Ws
+{
+    CONST HOST = '127.0.0.1';
+    CONST POST = 8812;
+
+    protected $fd;
+    protected $frame;
+
+    public static $ws;
+
+    public function __construct()
+    {
+        $ser = new Swoole\WebSocket\Server(self::HOST,self::POST);
+        self::$ws = $ser;
+        $this->on('open');
+        $this->on('message');
+    }
+
+
+    public function on($method){
+        self::$ws->on($method,[$this,'on_'.$method]);
+    }
+
+    public function on_open($server,$request)
+    {
+        echo "server: handshake success with fd-{$request->fd}";
+    }
+
+    public function on_message($server,$frame)
+    {
+        echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+        $this->frame = $frame;
+    }
+
+    public function push($message)
+    {
+        self::$ws->push($this->frame->fd,$message);
+        $this->close();
+    }
+
+
+    public function close()
+    {
+        self::$ws->on('close',function ($ser,$fd){
+            echo "client {$fd} closed\n";
+        });
+    }
+
+    public function start()
+    {
+        self::$ws->start();
+    }
+
+
+
+
+}
---
+++ demo/server/websocket/websocket.php
@@ -0,0 +1,11 @@
+<?php
+namespace demo\server\websocket;
+/**
+ * User: 侯光龙
+ * FileName: 文件名称
+ * Date: 2019/3/23
+ * Time: 22:05
+ */
+$ws = new Ws();
+$ws->push('my name is houguang');
+$ws->start();
