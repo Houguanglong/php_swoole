1bf6144cb3b7870199b81356974b4584b839d57f
Houguanglong <369380397@qq.com>
2019-03-24 15:31:06 +0800

    提交

---
+++ demo/io/Read.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: 侯光龙
+ * FileName: 文件名称
+ * Date: 2019/3/24
+ * Time: 15:26
+ */
+use Swoole\Coroutine as co;
+
+$filename = __DIR__.'/1.txt';
+co::create(function () use ($filename){
+    $r = co::readFile($filename);
+    if($r != false) {
+        echo "filecontent:{$r}";
+    }else{
+        var_dump($r);
+    }
+});
