d3358d4b4057163860a43892df4b9f81e68eebca
Houguanglong <369380397@qq.com>
2019-03-24 13:26:14 +0800

    提交

--- demo/server/websocket/WsTask.php
+++ demo/server/websocket/WsTask.php
@@ -5,7 +5,7 @@
  * Date: 2019/3/23
  * Time: 21:42
  */
-class Ws
+class WsTask
 {
     CONST HOST = '0.0.0.0'; //IP地址
 
@@ -16,8 +16,15 @@ class Ws
     public function __construct()
     {
         $this->ws = new Swoole\WebSocket\Server(self::HOST,self::POST);
+        $this->ws->set([
+            'worker_num'=>2,
+            'task_worker_num'=>2    //配置此参数后将会启用task功能
+        ]);
         $this->ws->on('open',[$this,'on_open']);
         $this->ws->on('message',[$this,'on_message']);
+        //使用task任务必须要注册task、finish回调函数
+        $this->ws->on('task',[$this,'on_task']);
+        $this->ws->on('finish',[$this,'on_finish']);
         $this->ws->on('close',[$this,'on_close']);
         $this->ws->start();
     }
@@ -41,7 +48,42 @@ class Ws
     public function on_message($server,$frame)
     {
         echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+        //投放task任务到task_worker任务池
+        $data = [
+            'task'=>1,
+            'fd'=>$frame->fd
+        ];
+        $server->taks($data);
+        //返回数据给客户端
         $server->push($frame->fd,'hello my name is houguang',1,true);
+    }
+
+    /**
+     * 执行task任务 返回执行结果到worker进程
+     * @param object $sev 服务器对象
+     * @param int $taskId task任务ID
+     * @param int $workId task任务进程ID
+     * @param array $data task任务数据
+     */
+    public function on_task($sev,$taskId,$workId,$data)
+    {
+        print_r($data);
+        //模拟执行10s场景
+        sleep(10);
+        //返回执行结果到worker进程 调用onFinish回调函数
+        return 'return_data_to_worker';
+    }
+
+    /**
+     * 将任务处理的结果发送给worker进程
+     * @param object $sev 服务器对象
+     * @param int $taskId 任务的ID
+     * @param $data task任务处理返回的结果内容
+     */
+    public function on_finish($sev,$taskId,$data)
+    {
+        echo "task_id={$taskId}\n";
+        echo "finish-return-data-to-work:{$data}\n";
     }
 
     /**
