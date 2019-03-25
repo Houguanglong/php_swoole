c7c59d76f2f0d2e42cae92b9d2181260b4366234
Houguanglong <369380397@qq.com>
2019-03-26 00:16:00 +0800

    提交

---
+++ demo/process/Process.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: 侯光龙
+ * FileName: 文件名称
+ * Date: 2019/3/26
+ * Time: 0:10
+ */
+$process = new swoole_process(function (swoole_process $pro){
+    $pro->exec('/home/learn/swoole/php/bin/php',[__DIR__.'/../server/HttpServer.php']);
+},false);
+
+$pid = $process->start();
+$process::wait();
+echo $pid.PHP_EOL;
