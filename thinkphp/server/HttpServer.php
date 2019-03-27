bb97fa5ea36693901441c305e43f0ea13057552f
Houguanglong <369380397@qq.com>
2019-03-27 16:01:43 +0800

    提交

--- thinkphp/server/HttpServer.php
+++ thinkphp/server/HttpServer.php
@@ -12,15 +12,50 @@ $http = new Server('0.0.0.0',8811);
 //设置请求访问静态资源自动查找文件目录
 //设置document_root并设置enable_static_handler为true后，
 //底层收到Http请求会先判断document_root路径下是否存在此文件
-//如果存在会直接发送文件内容给客户端，不再触发onRequest回调
+//如果存在会直接发送文件内容给客户端，不再触发onRequest回 调
 $http->set([
     'enable_static_handler'=>true,
-    'document_root'=>'/home/learn/bd_git/php_swoole/thinkphp/public/static'    //配置静态文件根目录
+    'document_root'=>'/home/learn/bd_git/php_swoole/thinkphp/public/static',    //配置静态文件根目录
+    'worker_num'=>5,
 ]);
 
+define('APP_PATH', __DIR__ . '/../application/');
+require __DIR__ . '/../thinkphp/base.php';
 //监听http请求 $request 为http请求对象 $response为http响应对象
-$http->on('request',function ($request,$response){
-    $response->end("<h1>Http-server</h1>".$request->get['age']); //end方法响应返回内容
+$http->on('request',function ($request,$response) use ($http){
+    if(isset($request->server)){
+        foreach ($request->server as $k =>$v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->header)){
+        foreach ($request->header as $k =>$v){
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->get)){
+        foreach ($request->get as $k =>$v){
+            $_GET[$k] = $v;
+        }
+    }
+
+    if(isset($request->post)){
+        foreach ($request->post as $k =>$v){
+            $_POST[$k] = $v;
+        }
+    }
+
+    ob_start();
+    // 执行应用并响应
+    think\Container::get('app', [APP_PATH])
+        ->run()
+        ->send();
+    $content = ob_get_contents();
+    ob_end_clean();
+    $response->end($content); //end方法响应返回内容
+    $http->close();
 });
 
 //开启httpserver服务
